42fcf26f6826a45d2909a6adb1394a066fc341b6
Kimaru thagana <macbuntu@192.168.1.3>
2021-02-24 20:03:32 +0300

    add imports to app.py

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ import pandas as pd
 import streamlit as st
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
+
+st.markdown('''
+# **The Streamlit X Pandas Crossover**
+ **EDA Web App** 
+''')
