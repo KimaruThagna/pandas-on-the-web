f5100a51cb4b79a65d12be0fd6f85d21bae1fbaa
Kimaru thagana <macbuntu@192.168.1.3>
2021-02-24 20:06:11 +0300

    process dataframe

--- app.py
+++ app.py
@@ -12,3 +12,17 @@ st.markdown('''
 # Upload CSV data
 with st.sidebar.header('1. Upload your CSV file'):
     uploaded_file = st.sidebar.file_uploader("Upload CSV file", type=["csv"])
+
+# Pandas Profiling Report
+if uploaded_file is not None:
+    @st.cache # make use of web cache
+    def load_csv():
+        csv = pd.read_csv(uploaded_file)
+        return csv
+    df = load_csv()
+    report = ProfileReport(df, explorative=True)
+    st.header('** DataFrame**')
+    st.write(df)
+    st.write('---')
+    st.header('**Pandas Profiling Results**')
+    st_profile_report(report)
